11c607d2aa45485622faf6e8fb6ae8c90655346b
Boris Marinov <boris.marinov99@gmail.com>
2024-09-21 00:48:55 +0300

    get single meal from db

--- src/app/meals/[slug]/page.tsx
+++ src/app/meals/[slug]/page.tsx
@@ -1,7 +1,45 @@
-export default function MealPage() {
+import Image from 'next/image';
+import styles from './page.module.css';
+import { getMeal } from '../../../../lib/meals';
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+
+interface IMealPageProps {
+    params: Params;
+}
+
+export default async function MealPage({ params }: IMealPageProps) {
+    const meal = await getMeal(params.slug);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
     return (
-        <h1>
-            Dynamic Meal
-        </h1>
+        <>
+            <header className={styles.header}>
+                <div className={styles.image}>
+                    <Image
+                        fill
+                        src={meal.image}
+                        alt={meal.title}
+                    />
+                </div>
+
+                <div className={styles.headerText}>
+                    <h1>{meal.title}</h1>
+
+                    <p className={styles.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+
+                    <p className={styles.summary}>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
+
+            <main>
+                <p className={styles.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions
+                }} />
+            </main>
+        </>
     )
 }
